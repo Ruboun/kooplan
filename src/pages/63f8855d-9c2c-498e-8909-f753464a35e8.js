393de1b3f402dc011001a0097e4aee0eb94e9103
Petr Andrýsek (via WeWeb) <pa.andrysek@gmail.com>
2026-04-27 13:50:32 +0200

    v323 - 10

---
+++ src/pages/63f8855d-9c2c-498e-8909-f753464a35e8.js
@@ -0,0 +1,58 @@
+// eslint-disable-next-line no-unused-vars
+import app from '@/_front/main.js';
+
+// eslint-disable-next-line no-undef
+import element_0dc461bb_103e_4b2e_80e0_846ec3c30a6e from '@/components/elements/element-0dc461bb-103e-4b2e-80e0-846ec3c30a6e/src/wwElement.vue';
+import element_14723a21_0178_4d92_a7e9_d1dfeaec29a7 from '@/components/elements/element-14723a21-0178-4d92-a7e9-d1dfeaec29a7/src/wwElement.vue';
+import element_1b1e2173_9b78_42cc_a8ee_a6167caea340 from '@/components/elements/element-1b1e2173-9b78-42cc-a8ee-a6167caea340/src/wwElement.vue';
+import element_1ba25bdf_dee9_4e0e_a0b8_b3f3128c3b65 from '@/components/elements/element-1ba25bdf-dee9-4e0e-a0b8-b3f3128c3b65/src/wwElement.vue';
+import element_2dff5957_3bd8_46f9_afa3_a5c5bab91931 from '@/components/elements/element-2dff5957-3bd8-46f9-afa3-a5c5bab91931/src/wwElement.vue';
+import element_3a7d6379_12d3_4387_98ff_b332bb492a63 from '@/components/elements/element-3a7d6379-12d3-4387-98ff-b332bb492a63/src/wwElement.vue';
+import element_53401515_b694_4c79_a88d_abeecb1de562 from '@/components/elements/element-53401515-b694-4c79-a88d-abeecb1de562/src/wwElement.vue';
+import element_57831abf_83ad_49ad_ba97_3bd30b035710 from '@/components/elements/element-57831abf-83ad-49ad-ba97-3bd30b035710/src/wwElement.vue';
+import element_59dca300_db78_42e4_a7a6_0cbf22d3cc82 from '@/components/elements/element-59dca300-db78-42e4-a7a6-0cbf22d3cc82/src/wwElement.vue';
+import element_6047b8df_81b7_45a7_a6b3_7355fb2fa3cd from '@/components/elements/element-6047b8df-81b7-45a7-a6b3-7355fb2fa3cd/src/wwElement.vue';
+import element_6145eb60_0af8_4e52_bcc6_dc0f6743654e from '@/components/elements/element-6145eb60-0af8-4e52-bcc6-dc0f6743654e/./src/wwElement_Select.vue';
+import element_6f8796b1_8273_498d_95fc_7013b7c63214 from '@/components/elements/element-6f8796b1-8273-498d-95fc-7013b7c63214/src/wwElement.vue';
+import element_83d890fb_84f9_4386_b459_fb4be89a8e15 from '@/components/elements/element-83d890fb-84f9-4386-b459-fb4be89a8e15/src/wwElement.vue';
+import element_97a63460_5c25_4d74_ac1f_86693c2e4a08 from '@/components/elements/element-97a63460-5c25-4d74-ac1f-86693c2e4a08/src/wwElement.vue';
+import element_9ecb2cfc_cef7_4be8_b736_3e17a3b7e9ff from '@/components/elements/element-9ecb2cfc-cef7-4be8-b736-3e17a3b7e9ff/src/wwElement.vue';
+import element_aa27b26f_0686_4c29_98c5_8217044045b7 from '@/components/elements/element-aa27b26f-0686-4c29-98c5-8217044045b7/src/wwElement.vue';
+import element_aa29a661_07ce_484e_8abb_456186211282 from '@/components/elements/element-aa29a661-07ce-484e-8abb-456186211282/src/wwElement.vue';
+import element_b783dc65_d528_4f74_8c14_e27c934c39b1 from '@/components/elements/element-b783dc65-d528-4f74-8c14-e27c934c39b1/src/wwElement.vue';
+import element_bf59c8e7_14c9_4c03_a739_8bd7d14031a4 from '@/components/elements/element-bf59c8e7-14c9-4c03-a739-8bd7d14031a4/src/wwElement.vue';
+import element_d7904e9d_fc9a_4d80_9e32_728e097879ad from '@/components/elements/element-d7904e9d-fc9a-4d80-9e32-728e097879ad/src/wwElement.vue';
+import element_deb10a01_5eef_4aa1_9017_1b51c2ad6fd0 from '@/components/elements/element-deb10a01-5eef-4aa1-9017-1b51c2ad6fd0/src/wwElement.vue';
+import element_e1e0ffa9_23af_471f_ac5a_6b49f508c653 from '@/components/elements/element-e1e0ffa9-23af-471f-ac5a-6b49f508c653/src/wwElement.vue';
+import element_e5e2f367_38d3_41fc_a938_7d221dd4691b from '@/components/elements/element-e5e2f367-38d3-41fc-a938-7d221dd4691b/src/wwElement.vue';
+
+// eslint-disable-next-line no-undef
+app.component('wwobject-0dc461bb-103e-4b2e-80e0-846ec3c30a6e', element_0dc461bb_103e_4b2e_80e0_846ec3c30a6e);
+app.component('wwobject-14723a21-0178-4d92-a7e9-d1dfeaec29a7', element_14723a21_0178_4d92_a7e9_d1dfeaec29a7);
+app.component('wwobject-1b1e2173-9b78-42cc-a8ee-a6167caea340', element_1b1e2173_9b78_42cc_a8ee_a6167caea340);
+app.component('wwobject-1ba25bdf-dee9-4e0e-a0b8-b3f3128c3b65', element_1ba25bdf_dee9_4e0e_a0b8_b3f3128c3b65);
+app.component('wwobject-2dff5957-3bd8-46f9-afa3-a5c5bab91931', element_2dff5957_3bd8_46f9_afa3_a5c5bab91931);
+app.component('wwobject-3a7d6379-12d3-4387-98ff-b332bb492a63', element_3a7d6379_12d3_4387_98ff_b332bb492a63);
+app.component('wwobject-53401515-b694-4c79-a88d-abeecb1de562', element_53401515_b694_4c79_a88d_abeecb1de562);
+app.component('wwobject-57831abf-83ad-49ad-ba97-3bd30b035710', element_57831abf_83ad_49ad_ba97_3bd30b035710);
+app.component('wwobject-59dca300-db78-42e4-a7a6-0cbf22d3cc82', element_59dca300_db78_42e4_a7a6_0cbf22d3cc82);
+app.component('wwobject-6047b8df-81b7-45a7-a6b3-7355fb2fa3cd', element_6047b8df_81b7_45a7_a6b3_7355fb2fa3cd);
+app.component('wwobject-6145eb60-0af8-4e52-bcc6-dc0f6743654e', element_6145eb60_0af8_4e52_bcc6_dc0f6743654e);
+app.component('wwobject-6f8796b1-8273-498d-95fc-7013b7c63214', element_6f8796b1_8273_498d_95fc_7013b7c63214);
+app.component('wwobject-83d890fb-84f9-4386-b459-fb4be89a8e15', element_83d890fb_84f9_4386_b459_fb4be89a8e15);
+app.component('wwobject-97a63460-5c25-4d74-ac1f-86693c2e4a08', element_97a63460_5c25_4d74_ac1f_86693c2e4a08);
+app.component('wwobject-9ecb2cfc-cef7-4be8-b736-3e17a3b7e9ff', element_9ecb2cfc_cef7_4be8_b736_3e17a3b7e9ff);
+app.component('wwobject-aa27b26f-0686-4c29-98c5-8217044045b7', element_aa27b26f_0686_4c29_98c5_8217044045b7);
+app.component('wwobject-aa29a661-07ce-484e-8abb-456186211282', element_aa29a661_07ce_484e_8abb_456186211282);
+app.component('wwobject-b783dc65-d528-4f74-8c14-e27c934c39b1', element_b783dc65_d528_4f74_8c14_e27c934c39b1);
+app.component('wwobject-bf59c8e7-14c9-4c03-a739-8bd7d14031a4', element_bf59c8e7_14c9_4c03_a739_8bd7d14031a4);
+app.component('wwobject-d7904e9d-fc9a-4d80-9e32-728e097879ad', element_d7904e9d_fc9a_4d80_9e32_728e097879ad);
+app.component('wwobject-deb10a01-5eef-4aa1-9017-1b51c2ad6fd0', element_deb10a01_5eef_4aa1_9017_1b51c2ad6fd0);
+app.component('wwobject-e1e0ffa9-23af-471f-ac5a-6b49f508c653', element_e1e0ffa9_23af_471f_ac5a_6b49f508c653);
+app.component('wwobject-e5e2f367-38d3-41fc-a938-7d221dd4691b', element_e5e2f367_38d3_41fc_a938_7d221dd4691b);
+
+// eslint-disable-next-line no-undef
+import section_99586bd3_2b15_4d6b_a025_6a50d07ca845 from '@/components/sections/section-99586bd3-2b15-4d6b-a025-6a50d07ca845/src/wwSection.vue';
+
+// eslint-disable-next-line no-undef
+app.component('section-99586bd3-2b15-4d6b-a025-6a50d07ca845', section_99586bd3_2b15_4d6b_a025_6a50d07ca845);
---
+++ vite.config.js
@@ -0,0 +1,92 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import autoprefixer from 'autoprefixer';
+import path from 'path';
+import fs from 'fs';
+import { parseEnv } from 'node:util';
+import handlebars from 'handlebars';
+
+const pages = {"da77339e-2b38-45a5-bee1-b6b5e2b3cbb4-en":{"outputDir":"./nastaveni","lang":"en","cacheVersion":323,"meta":[{"name":"twitter:card","content":"summary"},{"property":"og:type","content":"website"},{"name":"robots","content":"index, follow"}],"scripts":{"head":"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js\"></script>\r\n<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js\"></script>\r\n\r\n<style>\r\n    /* Na obrazovce může být print-root klidně skrytý */\r\n    @media screen {\r\n        #print-root {\r\n            display: none;\r\n        }\r\n    }\r\n\r\n    /* Tyto styly používá html2pdf při renderu */\r\n    #print-root {\r\n        width: 190mm;\r\n        margin: 0 auto;\r\n        padding: 0;\r\n        background: #fff;\r\n    }\r\n\r\n    #print-root .print-item {\r\n        break-inside: avoid;\r\n        page-break-inside: avoid;\r\n        margin-bottom: 8mm;\r\n    }\r\n\r\n    #print-root .print-break {\r\n        break-before: page;\r\n        page-break-before: always;\r\n    }\r\n</style>\n","body":"\n"},"baseTag":{"href":"/","target":"_self"},"alternateLinks":[{"rel":"alternate","hreflang":"x-default","href":"https://bd6c9360-77d1-4f21-bfb4-e9473a7c2ea4.weweb-preview.io/nastaveni/"},{"rel":"alternate","hreflang":"en","href":"https://bd6c9360-77d1-4f21-bfb4-e9473a7c2ea4.weweb-preview.io/nastaveni/"}]},"30fedf6b-5f95-4f06-93e3-ece146627e39-en":{"outputDir":"./registrace","lang":"en","cacheVersion":323,"meta":[{"name":"twitter:card","content":"summary"},{"property":"og:type","content":"website"},{"name":"robots","content":"index, follow"}],"scripts":{"head":"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js\"></script>\r\n<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js\"></script>\r\n\r\n<style>\r\n    /* Na obrazovce může být print-root klidně skrytý */\r\n    @media screen {\r\n        #print-root {\r\n            display: none;\r\n        }\r\n    }\r\n\r\n    /* Tyto styly používá html2pdf při renderu */\r\n    #print-root {\r\n        width: 190mm;\r\n        margin: 0 auto;\r\n        padding: 0;\r\n        background: #fff;\r\n    }\r\n\r\n    #print-root .print-item {\r\n        break-inside: avoid;\r\n        page-break-inside: avoid;\r\n        margin-bottom: 8mm;\r\n    }\r\n\r\n    #print-root .print-break {\r\n        break-before: page;\r\n        page-break-before: always;\r\n    }\r\n</style>\n","body":"\n"},"baseTag":{"href":"/","target":"_self"},"alternateLinks":[{"rel":"alternate","hreflang":"x-default","href":"https://bd6c9360-77d1-4f21-bfb4-e9473a7c2ea4.weweb-preview.io/registrace/"},{"rel":"alternate","hreflang":"en","href":"https://bd6c9360-77d1-4f21-bfb4-e9473a7c2ea4.weweb-preview.io/registrace/"}]},"c57d39a0-74c0-42f4-90cb-cca890f55c63-en":{"outputDir":"./planzivota","lang":"en","cacheVersion":323,"meta":[{"name":"twitter:card","content":"summary"},{"property":"og:type","content":"website"},{"name":"robots","content":"index, follow"}],"scripts":{"head":"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js\"></script>\r\n<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js\"></script>\r\n\r\n<style>\r\n    /* Na obrazovce může být print-root klidně skrytý */\r\n    @media screen {\r\n        #print-root {\r\n            display: none;\r\n        }\r\n    }\r\n\r\n    /* Tyto styly používá html2pdf při renderu */\r\n    #print-root {\r\n        width: 190mm;\r\n        margin: 0 auto;\r\n        padding: 0;\r\n        background: #fff;\r\n    }\r\n\r\n    #print-root .print-item {\r\n        break-inside: avoid;\r\n        page-break-inside: avoid;\r\n        margin-bottom: 8mm;\r\n    }\r\n\r\n    #print-root .print-break {\r\n        break-before: page;\r\n        page-break-before: always;\r\n    }\r\n</style>\n<script src=\"https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js\"></script>","body":"\n<style>\r\n@media print {\r\n    /* Vynutí barvy pozadí a grafiku i v PDF */\r\n    * { \r\n        -webkit-print-color-adjust: exact !important; \r\n        print-color-adjust: exact !important; \r\n    }\r\n\r\n    /* Nastavení pro váš hlavní kontejner */\r\n    #export-oblast {\r\n        background-color: #F8F9FA !important; /* Šedobílé pozadí z vašeho webu */\r\n        padding: 20px !important;\r\n        width: 100% !important;\r\n    }\r\n\r\n    /* Aby se karty (Zdraví, Auto, atd.) nerozsekly v půlce na dvě stránky */\r\n    .card, [class*=\"card\"], [class*=\"section\"] { \r\n        page-break-inside: avoid !important;\r\n        break-inside: avoid !important;\r\n        margin-bottom: 20px !important;\r\n    }\r\n}\r\n</style>"},"baseTag":{"href":"/","target":"_self"},"alternateLinks":[{"rel":"alternate","hreflang":"x-default","href":"https://bd6c9360-77d1-4f21-bfb4-e9473a7c2ea4.weweb-preview.io/planzivota/"},{"rel":"alternate","hreflang":"en","href":"https://bd6c9360-77d1-4f21-bfb4-e9473a7c2ea4.weweb-preview.io/planzivota/"}]},"97018d21-11cf-4343-b2fa-bc29165d1431-en":{"outputDir":"./","lang":"en","cacheVersion":323,"meta":[{"name":"twitter:card","content":"summary"},{"property":"og:type","content":"website"},{"name":"robots","content":"index, follow"}],"scripts":{"head":"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js\"></script>\r\n<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js\"></script>\r\n\r\n<style>\r\n    /* Na obrazovce může být print-root klidně skrytý */\r\n    @media screen {\r\n        #print-root {\r\n            display: none;\r\n        }\r\n    }\r\n\r\n    /* Tyto styly používá html2pdf při renderu */\r\n    #print-root {\r\n        width: 190mm;\r\n        margin: 0 auto;\r\n        padding: 0;\r\n        background: #fff;\r\n    }\r\n\r\n    #print-root .print-item {\r\n        break-inside: avoid;\r\n        page-break-inside: avoid;\r\n        margin-bottom: 8mm;\r\n    }\r\n\r\n    #print-root .print-break {\r\n        break-before: page;\r\n        page-break-before: always;\r\n    }\r\n</style>\n","body":"\n"},"baseTag":{"href":"/","target":"_self"},"alternateLinks":[{"rel":"alternate","hreflang":"x-default","href":"https://bd6c9360-77d1-4f21-bfb4-e9473a7c2ea4.weweb-preview.io/"},{"rel":"alternate","hreflang":"en","href":"https://bd6c9360-77d1-4f21-bfb4-e9473a7c2ea4.weweb-preview.io/"}]},"0ed43827-1bdf-41f5-9286-299de6a013b2-en":{"outputDir":"./duchodplan","lang":"en","cacheVersion":323,"meta":[{"name":"twitter:card","content":"summary"},{"property":"og:type","content":"website"},{"name":"robots","content":"index, follow"}],"scripts":{"head":"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js\"></script>\r\n<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js\"></script>\r\n\r\n<style>\r\n    /* Na obrazovce může být print-root klidně skrytý */\r\n    @media screen {\r\n        #print-root {\r\n            display: none;\r\n        }\r\n    }\r\n\r\n    /* Tyto styly používá html2pdf při renderu */\r\n    #print-root {\r\n        width: 190mm;\r\n        margin: 0 auto;\r\n        padding: 0;\r\n        background: #fff;\r\n    }\r\n\r\n    #print-root .print-item {\r\n        break-inside: avoid;\r\n        page-break-inside: avoid;\r\n        margin-bottom: 8mm;\r\n    }\r\n\r\n    #print-root .print-break {\r\n        break-before: page;\r\n        page-break-before: always;\r\n    }\r\n</style>\n<script src=\"https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js\"></script>","body":"\n<style>\r\n@media print {\r\n    /* Vynutí barvy pozadí a grafiku i v PDF */\r\n    * { \r\n        -webkit-print-color-adjust: exact !important; \r\n        print-color-adjust: exact !important; \r\n    }\r\n\r\n    /* Nastavení pro váš hlavní kontejner */\r\n    #export-oblast {\r\n        background-color: #F8F9FA !important; /* Šedobílé pozadí z vašeho webu */\r\n        padding: 20px !important;\r\n        width: 100% !important;\r\n    }\r\n\r\n    /* Aby se karty (Zdraví, Auto, atd.) nerozsekly v půlce na dvě stránky */\r\n    .card, [class*=\"card\"], [class*=\"section\"] { \r\n        page-break-inside: avoid !important;\r\n        break-inside: avoid !important;\r\n        margin-bottom: 20px !important;\r\n    }\r\n}\r\n</style>"},"baseTag":{"href":"/","target":"_self"},"alternateLinks":[{"rel":"alternate","hreflang":"x-default","href":"https://bd6c9360-77d1-4f21-bfb4-e9473a7c2ea4.weweb-preview.io/duchodplan/"},{"rel":"alternate","hreflang":"en","href":"https://bd6c9360-77d1-4f21-bfb4-e9473a7c2ea4.weweb-preview.io/duchodplan/"}]},"06023897-70ec-4680-bace-cfd329217f99-en":{"outputDir":"./call-journal","lang":"en","cacheVersion":323,"meta":[{"name":"twitter:card","content":"summary"},{"property":"og:type","content":"website"},{"name":"robots","content":"index, follow"}],"scripts":{"head":"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js\"></script>\r\n<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js\"></script>\r\n\r\n<style>\r\n    /* Na obrazovce může být print-root klidně skrytý */\r\n    @media screen {\r\n        #print-root {\r\n            display: none;\r\n        }\r\n    }\r\n\r\n    /* Tyto styly používá html2pdf při renderu */\r\n    #print-root {\r\n        width: 190mm;\r\n        margin: 0 auto;\r\n        padding: 0;\r\n        background: #fff;\r\n    }\r\n\r\n    #print-root .print-item {\r\n        break-inside: avoid;\r\n        page-break-inside: avoid;\r\n        margin-bottom: 8mm;\r\n    }\r\n\r\n    #print-root .print-break {\r\n        break-before: page;\r\n        page-break-before: always;\r\n    }\r\n</style>\n","body":"\n"},"baseTag":{"href":"/","target":"_self"},"alternateLinks":[{"rel":"alternate","hreflang":"x-default","href":"https://bd6c9360-77d1-4f21-bfb4-e9473a7c2ea4.weweb-preview.io/call-journal/"},{"rel":"alternate","hreflang":"en","href":"https://bd6c9360-77d1-4f21-bfb4-e9473a7c2ea4.weweb-preview.io/call-journal/"}]},"63f8855d-9c2c-498e-8909-f753464a35e8-en":{"outputDir":"./home","lang":"en","cacheVersion":323,"meta":[{"name":"twitter:card","content":"summary"},{"property":"og:type","content":"website"},{"name":"robots","content":"index, follow"}],"scripts":{"head":"<script src=\"https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js\"></script>\r\n<script src=\"https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js\"></script>\r\n\r\n<style>\r\n    /* Na obrazovce může být print-root klidně skrytý */\r\n    @media screen {\r\n        #print-root {\r\n            display: none;\r\n        }\r\n    }\r\n\r\n    /* Tyto styly používá html2pdf při renderu */\r\n    #print-root {\r\n        width: 190mm;\r\n        margin: 0 auto;\r\n        padding: 0;\r\n        background: #fff;\r\n    }\r\n\r\n    #print-root .print-item {\r\n        break-inside: avoid;\r\n        page-break-inside: avoid;\r\n        margin-bottom: 8mm;\r\n    }\r\n\r\n    #print-root .print-break {\r\n        break-before: page;\r\n        page-break-before: always;\r\n    }\r\n</style>\n","body":"\n"},"baseTag":{"href":"/","target":"_self"},"alternateLinks":[{"rel":"alternate","hreflang":"x-default","href":"https://bd6c9360-77d1-4f21-bfb4-e9473a7c2ea4.weweb-preview.io/home/"},{"rel":"alternate","hreflang":"en","href":"https://bd6c9360-77d1-4f21-bfb4-e9473a7c2ea4.weweb-preview.io/home/"}]}};
+
+// Read the main HTML template
+const template = fs.readFileSync(path.resolve(__dirname, 'template.html'), 'utf-8');
+const compiledTemplate = handlebars.compile(template);
+
+// Generate an HTML file for each page with its metadata
+Object.values(pages).forEach(pageConfig => {
+    // Compile the template with page metadata
+    const html = compiledTemplate({
+        title: pageConfig.title,
+        lang: pageConfig.lang,
+        meta: pageConfig.meta,
+        structuredData: pageConfig.structuredData || null,
+        scripts: {
+            head: pageConfig.scripts.head,
+            body: pageConfig.scripts.body,
+        },
+        alternateLinks: pageConfig.alternateLinks,
+        cacheVersion: pageConfig.cacheVersion,
+        baseTag: pageConfig.baseTag,
+    });
+
+    // Save output html for each page
+    if (!fs.existsSync(pageConfig.outputDir)) {
+        fs.mkdirSync(pageConfig.outputDir, { recursive: true });
+    }
+    fs.writeFileSync(`${pageConfig.outputDir}/index.html`, html);
+});
+
+const rolldownOptionsInput = {};
+for (const pageName in pages) {
+    rolldownOptionsInput[pageName] = path.resolve(__dirname, pages[pageName].outputDir, 'index.html');
+}
+
+function getFrontEnvironmentValues(root, mode) {
+    const filePath = path.resolve(root, `.env.${mode}`);
+    if (!fs.existsSync(filePath)) {
+        return {};
+    }
+
+    return Object.fromEntries(Object.entries(parseEnv(fs.readFileSync(filePath, 'utf8'))).filter(([key]) => !key.startsWith('VITE_')));
+}
+
+export default defineConfig(() => {
+    return {
+        plugins: [vue()],
+        base: "/",
+        define: {
+            global: 'globalThis',
+            __WW_FRONT_ENV_VARIABLES__: JSON.stringify({
+                staging: getFrontEnvironmentValues(__dirname, 'staging'),
+                production: getFrontEnvironmentValues(__dirname, 'production'),
+            }),
+        },
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    api: 'modern-compiler',
+                },
+            },
+            postcss: {
+                plugins: [autoprefixer],
+            },
+        },
+        build: {
+            chunkSizeWarningLimit: 10000,
+            rolldownOptions: {
+                input: rolldownOptionsInput,
+                onwarn: (entry, next) => {
+                    if (entry.loc?.file && /js$/.test(entry.loc.file) && /Use of eval in/.test(entry.message)) return;
+                    if (/Use of direct `eval`/.test(entry.message)) return;
+                    return next(entry);
+                },
+            },
+        },
+        logLevel: 'warn',
+    };
+});
